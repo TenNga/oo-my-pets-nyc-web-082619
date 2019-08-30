1c9e471c34c35d3854324bfcb831e84d1c7bb07e
Kevin Karma <kevinkarma55@gmail.com>
2019-08-30 02:48:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,8 @@
 class Owner
   attr_reader :name 
-  def initialize (name)
+  def initialize (name, species = "human")
     @name = name 
+    @species = species
   end
   
-  def species
-    
-  end
 end
